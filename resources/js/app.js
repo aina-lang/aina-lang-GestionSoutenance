57e7e91974ab136163a504c7f853be3be17e526f
aina-lang <merciaaina@gmail.com>
2023-04-03 09:40:41 -0400

    progression

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 import './bootstrap';
-
+import 'select2';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus';
 window.Alpine = Alpine;
@@ -7,3 +7,7 @@ window.Alpine = Alpine;
 Alpine.plugin(focus);
 
 Alpine.start();
+
+$(document).ready(function() {
+    $('select').select2();
+});
---
+++ resources/js/script.js
@@ -0,0 +1,7 @@
+
+
+
+
+var f=function(params) {
+    console.log("yesssssssssss");
+}
